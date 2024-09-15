9f8625b6f0160ac74a31aa1d2205a7c7465a7bc5
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-09-15 19:59:52 +0700

    login-toast

---
+++ login.js
@@ -0,0 +1,77 @@
+const loginUser = document.getElementById("login-username");
+const loginPassword = document.getElementById("login-password");
+const loginForm = document.getElementById("loginForm");
+const login = document.getElementById("login");
+
+login.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("Login attempt");
+
+  let accounts = JSON.parse(localStorage.getItem("accounts")) || [];
+  console.log(accounts);
+  console.log(loginUser.value);
+  console.log(loginPassword.value);
+  for (let i = 0; i < accounts.length; i++) {
+    if (
+      loginUser.value === accounts[i].username &&
+      loginPassword.value === accounts[i].password
+    ) {
+      toast({
+        title: "Thông báo ✅✅✅",
+        message: `${loginUser.value} đã đăng ký thành công!`,
+        type: "success",
+        duration: 2000,
+      });
+      setTimeout(() => {
+        window.location.href = "index.html";
+      }, 2000);
+      return;
+    }
+  }
+});
+
+function toast({ title = "", message = "", type = "info", duration = 3000 }) {
+  const main = document.getElementById("toast");
+  if (main) {
+    const toast = document.createElement("div");
+
+    // Auto remove toast
+    const autoRemoveId = setTimeout(function () {
+      main.removeChild(toast);
+    }, duration + 1000);
+
+    // Remove toast when clicked
+    toast.onclick = function (e) {
+      if (e.target.closest(".toast__close")) {
+        main.removeChild(toast);
+        clearTimeout(autoRemoveId);
+      }
+    };
+
+    const icons = {
+      success: "fas fa-check-circle",
+      info: "fas fa-info-circle",
+      warning: "fas fa-exclamation-circle",
+      error: "fas fa-exclamation-circle",
+    };
+    const icon = icons[type];
+    const delay = (duration / 1000).toFixed(2);
+
+    toast.classList.add("toast", `toast--${type}`);
+    toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
+
+    toast.innerHTML = `
+                    <div class="toast__icon">
+                        <i class="${icon}"></i>
+                    </div>
+                    <div class="toast__body">
+                        <h3 class="toast__title">${title}</h3>
+                        <p class="toast__msg">${message}</p>
+                    </div>
+                    <div class="toast__close">
+                        <i class="fas fa-times"></i>
+                    </div>
+                `;
+    main.appendChild(toast);
+  }
+}
--- script.js
+++ script.js
@@ -30,7 +30,12 @@ form.addEventListener("submit", function (event) {
     confirmError.style.display = "block";
     confirmError.innerHTML += "Confirm Password has to be the same as Password";
   }
-  if (usernameError.innerHTML !== "" || confirmPassword.innerHTML !== "" ||passwordError.innerHTML !== "") return;
+  if (
+    usernameError.innerHTML !== "" ||
+    confirmPassword.innerHTML !== "" ||
+    passwordError.innerHTML !== ""
+  )
+    return;
 
   console.log(
     `Đăng ký thành công ✅
@@ -39,36 +44,15 @@ form.addEventListener("submit", function (event) {
       - ${confirmPassword.value}`
   );
 
-
-
   let accounts = JSON.parse(localStorage.getItem("accounts")) || [];
   accounts.push({
-  username: userName.value,
-  password: password.value
-});
+    username: userName.value,
+    password: password.value,
+  });
 
-localStorage.setItem("accounts", JSON.stringify(accounts));
+  localStorage.setItem("accounts", JSON.stringify(accounts));
 
-userName.value = "";
-password.value = "";
-confirmPassword.value = "";
-
-}); 
-
-const loginUser = document.getElementById("login-username");
-const loginPassword = document.getElementById("login-password");
-const loginForm = document.getElementById("loginForm");
-const login = document.getElementById("login");
-
-login.addEventListener("click", function (event){
-  event.preventDefault();
-  let accounts = JSON.parse(localStorage.getItem("accounts")) || [];
-  for (let i = 0; i <=accounts.length-1; i += 1) {
-    console.log(accounts[i]);
-    if (loginUser.value == userName.value && loginPassword.value == loginPassword) {
-      // return true;
-    } else {
-      // return false;
-    }
-  }
+  userName.value = "";
+  password.value = "";
+  confirmPassword.value = "";
 });
