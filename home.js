33cbf73e449b64b99a24a4495bbcbdbccacdcd26
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-09-30 19:57:57 +0700

    filtẻ

--- home.js
+++ home.js
@@ -1,5 +1,4 @@
-import { radio_list } from "./data.js";
-import { album_list } from "./data.js";
+import { radio_list, album_list } from "./data.js";
 // Check khi đăng nhập vào rồi thì không cần phải đăng nhập lại nữa ( không vào trang login nữa)
 function displayCurrentUser() {
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
@@ -9,8 +8,9 @@ function displayCurrentUser() {
     window.location.href = "login.html";
   }
   listRadioPopulation();
-  smallList();
+  smallList(album_list);
 }
+
 function listRadioPopulation() {
   const radioList = document.getElementById("list-card");
   for (let i = 0; i < radio_list.length; i++) {
@@ -23,17 +23,33 @@ function listRadioPopulation() {
               </div>`;
   }
 }
-function smallList() {
-  const sideAlbum = document.getElementById("albums");
-  for (let i = 0; i < album_list.length; i++) {
-    sideAlbum.innerHTML += `<div class="album-demo">
-                  <img src="${album_list[i].alb_img}" />
-                  <div class="album-info">
-                    <p class="name">${album_list[i].name}</p>
-                    <p class="type">Playlist | ${album_list[i].year}</p>
-                  </div>`;
 
+function smallList(album) {
+  const sideAlbum = document.getElementById("albums");
+  sideAlbum.innerHTML = "";
+  for (let i = 0; i < album.length; i++) {
+    sideAlbum.innerHTML += `<div class="album-demo">
+                  <img src="${album[i].alb_img}" />
+                  <div class="album-info">
+                    <p class="name">${album[i].name}</p>
+                    <p class="type">Playlist | ${album[i].year}</p>
+                  </div>`;
   }
 }
+
+const searchValue = document.getElementById("search-ablum");
+searchValue.addEventListener("input", function (e) {
+  let valueSearch = e.target.value;
+  let album = [];
+  for (let i = 0; i < album_list.length; i++) {
+    console.log(`${album_list[i].name} - ${valueSearch}`);
+    if (album_list[i].name.includes(valueSearch)) {
+      album.push(album_list[i]);
+    }
+  }
+  console.log(album);
+  smallList(album);
+});
+
 displayCurrentUser();
 console.log(album_list);
